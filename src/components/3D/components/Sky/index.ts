10cb066226db54d97879e497cf854f2bb3cfc52b
Leonid <45781017+LeonidGrr@users.noreply.github.com>
2022-07-14 17:15:23 +0300

    Update index.ts

--- src/components/3D/components/Sky/index.ts
+++ src/components/3D/components/Sky/index.ts
@@ -10,11 +10,11 @@ export const Sky = async (renderer: THREE.WebGLRenderer, scene: THREE.Scene, cam
     let sun = new THREE.Vector3();
 
     const effectController = {
-        turbidity: 0,
-        rayleigh: 0,
+        turbidity: -1.0,
+        rayleigh: -1.0,
         mieCoefficient: 0.005,
         mieDirectionalG: 0.7,
-        elevation: 0,
+        elevation: -1.0,
         azimuth: -160,
         exposure: 1.25,
     };
@@ -36,20 +36,25 @@ export const Sky = async (renderer: THREE.WebGLRenderer, scene: THREE.Scene, cam
 
     const render = () => {
         if (theme.mode === SceneThemeMode.DAY) {
-            if (effectController.elevation !== 9.3 || effectController.turbidity !== 10 || effectController.rayleigh !== 3) {
-                sky.visible = true;
+            if (effectController.elevation < 9.3) {
                 effectController.elevation = THREE.MathUtils.lerp(effectController.elevation, 9.3, 0.05);
-                effectController.turbidity = THREE.MathUtils.lerp(effectController.turbidity, 10, 0.05);
-                effectController.rayleigh = THREE.MathUtils.lerp(effectController.rayleigh, 3, 0.05);
+            }
+            if (effectController.turbidity < 10) {
+                effectController.turbidity = THREE.MathUtils.lerp(effectController.turbidity, 10.0, 0.05);
+            }
+            if (effectController.rayleigh < 3) {
+                effectController.rayleigh = THREE.MathUtils.lerp(effectController.rayleigh, 3.0, 0.05);
             }
         }
         if (theme.mode === SceneThemeMode.NIGHT) {
-            if (effectController.elevation > 0 || effectController.rayleigh > 0) {
-                effectController.elevation = THREE.MathUtils.lerp(effectController.elevation, 0, 0.05);
-                effectController.turbidity = THREE.MathUtils.lerp(effectController.turbidity, 0, 0.05);
-                effectController.rayleigh = THREE.MathUtils.lerp(effectController.rayleigh, 0, 0.05);
-            } else {
-                sky.visible = false;
+            if (effectController.elevation > -1) {
+                effectController.elevation = THREE.MathUtils.lerp(effectController.elevation, -1.0, 0.02);
+            }
+            if (effectController.turbidity > -1) {
+                effectController.turbidity = THREE.MathUtils.lerp(effectController.turbidity, -1.0, 0.05);
+            }
+            if (effectController.rayleigh > -1) {
+                effectController.rayleigh = THREE.MathUtils.lerp(effectController.rayleigh, -1.0, 0.05);
             }
         }
         init();
